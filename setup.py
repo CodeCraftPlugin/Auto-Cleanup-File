3758ad09f640ffdd7ca4a851dfe545fe119cf28f
CodeCraftPlugin <86466022+CodeCraftPlugin@users.noreply.github.com>
2024-01-10 18:54:40 +0530

    added actions to the project

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from cx_Freeze import setup, Executable
 import sys
+import os
+version = 'v1.2.0'
 
+
+os.system(f"echo '::set-output name=version::{version}'")
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 build_options = {'packages': ['os', 'getpass', 'library'], 'excludes': [], 'build_exe': 'build_windows',
@@ -11,8 +15,9 @@ base = 'Win32GUI' if sys.platform == 'win32' else None
 executables = [
     Executable('auto_download_sorter.py', base=base, target_name='Auto Download Sorter', icon='icon.ico')
 ]
+
 setup(name='Auto Download Sorter',
-      version='1.0',
+      version=version,
       description='Auto Download Sorter',
       options={'build_exe': build_options},
       executables=executables)
